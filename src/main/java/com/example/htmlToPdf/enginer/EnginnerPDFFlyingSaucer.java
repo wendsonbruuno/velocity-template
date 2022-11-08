e5902ea2cac1af3ce8129c59c6f970835927fecf
Fábio Acioli Ribas <faciolir@nttdata.com>
2022-11-08 03:37:49 -0300

    novo layout

--- src/main/java/com/example/htmlToPdf/enginer/EnginnerPDFFlyingSaucer.java
+++ src/main/java/com/example/htmlToPdf/enginer/EnginnerPDFFlyingSaucer.java
@@ -49,44 +49,55 @@ public class EnginnerPDFFlyingSaucer {
 		context.put("ouvidoria", "0800 7769595");
 		context.put("email", "pan@pan.com.br");
 		context.put("data", "24/03/2022");
+
 		context.put("nome", "LIGIA APARECIDA FRANCISCO ALBANEZ");
-		context.put("cpf", "33801206858");
+		context.put("cnpj-cpf", "33801206858");
 		context.put("endereco-cliente", "AVENIDA ALBERT BARTHOLOME, 172 AP 138 TOR A");
-		context.put("agencia", "0019-MATRIZ");
-		context.put("conta", "4400003753");
 		context.put("cep", "05541000");
 		context.put("cidade", "SAO PAULO ");
 		context.put("bairro", "JARDIM DAS VERTENTE");
-		context.put("telefone", "(09) 67882227");
-		context.put("total-aplicado", "83.040,02");
-		context.put("saldo-atual-bruto", "85.870,73");
-		context.put("iof-atual", "65,26");
-		context.put("saldo-atual-liquido", "85.805,47");
-		context.put("saldo-bloq-jud", "0,00");
-		context.put("saldo-bloq-out", "25,01");
-		context.put("saldo-bruto-disponivel", "85.845,44");
-		
-		
-		Map<String, String> item = new HashMap<>();
-		item.put("papel", "GUARDAR DINHEIRO");
-		item.put("nota", "1040166");
-		item.put("indicador-taxa", "103,00% CDI / 0,00");
-		item.put("emissao", "17/05/2021");
-		item.put("vencimento", "17/05/2021");
-		item.put("valor-aplicado", "35.000,00");
-		item.put("saldo-bruto", "37.635,85");
-		item.put("ir", "31,95");
-		item.put("iof", "31,95");
-		item.put("saldo-liquido", "37.635,85");
-		item.put("liquidez", "25/01/2024");
-		
-		List<Map<String, String>> lista = new ArrayList<>(); 
-		
-		for (int i = 0; i < 15; i++) {
-			lista.add(item);
-		}
-		
-		context.put("lista", lista);
+
+		context.put("importante",
+				"Devem estar formalizados neste documento, compromissos de recompra ou compra caso existente.\n Este documento é intransferível e inegociável. \n Informamos para todos os fins que o controle e acompanhamento das datas de vencimento das aplicações em depósito a prazo realizado pelos clientes são de sua\r\n"
+						+ "inteira responsabilidade.");
+
+		context.put("observacoes", "EMISSAO POUPA PAN");
+
+		Map<String, String> itemCaracteristicaTitulo = new HashMap<>();
+		itemCaracteristicaTitulo.put("titulo", "GUARDAR DINHEIRO");
+		itemCaracteristicaTitulo.put("emissao", "23/08/2022");
+		itemCaracteristicaTitulo.put("vencimento", "21/08/2023");
+		itemCaracteristicaTitulo.put("local-custodia", "CRT4");
+		itemCaracteristicaTitulo.put("indexador", "CDI");
+		itemCaracteristicaTitulo.put("percentual", "111,0000");
+		itemCaracteristicaTitulo.put("taxa-nominal", "0,0000");
+		itemCaracteristicaTitulo.put("codigo-if", "");
+
+		context.put("caracteristica-titulo", itemCaracteristicaTitulo);
+
+		Map<String, String> itemCaracteristicaoOperacao = new HashMap<>();
+		itemCaracteristicaoOperacao.put("quantidade", "400");
+		itemCaracteristicaoOperacao.put("pu-operacao", "0,01000000");
+		itemCaracteristicaoOperacao.put("forma-liquidacao", "CONTA CORRENTE");
+		itemCaracteristicaoOperacao.put("valor-bruto", "4,00");
+		itemCaracteristicaoOperacao.put("if", "");
+		itemCaracteristicaoOperacao.put("iof", "111,0000");
+		itemCaracteristicaoOperacao.put("valor-liquido", "4,00");
+		itemCaracteristicaoOperacao.put("condicao-resgate", "M - Condição de resgate antecipado");
+
+		context.put("caracteristica-operacao", itemCaracteristicaoOperacao);
+
+		Map<String, String> itemCaracteristicaoCompromisso = new HashMap<>();
+		itemCaracteristicaoCompromisso.put("taxa", "0,000000%");
+		itemCaracteristicaoCompromisso.put("prazo", "0");
+		itemCaracteristicaoCompromisso.put("pu-retorno", "0,00000000");
+		itemCaracteristicaoCompromisso.put("indexador", "0,000%");
+		itemCaracteristicaoCompromisso.put("vencimento", "");
+		itemCaracteristicaoCompromisso.put("valor-retorno-bruto", "0,00");
+		itemCaracteristicaoCompromisso.put("ir-retido", "0,00");
+		itemCaracteristicaoCompromisso.put("valor-retorno-liquido", "0,00");
+
+		context.put("caracteristica-compromisso", itemCaracteristicaoCompromisso);
 	}
 
 	private static void initEngine(VelocityEngine ve) {
@@ -102,9 +113,11 @@ public class EnginnerPDFFlyingSaucer {
 		return document.html();
 	}
 
-	private static ByteArrayOutputStream xhtmlToPdf(String xhtml, String outFileName) throws IOException, DocumentException {
+	private static ByteArrayOutputStream xhtmlToPdf(String xhtml, String outFileName)
+			throws IOException, DocumentException {
 		ITextRenderer iTextRenderer = new ITextRenderer(20f * 4f / 3f, 20);
-		iTextRenderer.getSharedContext().setReplacedElementFactory(new MediaReplacedElementFactory(iTextRenderer.getSharedContext().getReplacedElementFactory()));
+		iTextRenderer.getSharedContext().setReplacedElementFactory(
+				new MediaReplacedElementFactory(iTextRenderer.getSharedContext().getReplacedElementFactory()));
 		iTextRenderer.setDocumentFromString(xhtml);
 		iTextRenderer.layout();
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
